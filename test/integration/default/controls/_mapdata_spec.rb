37eaca38b0394f035dc1cdb7c9ea1203d357206a
Imran Iqbal <iqbalmy@hotmail.com>
2020-08-26 22:58:52 +0100

    test(_mapdata_spec): perform comparison using `describe yaml` instead

--- test/integration/default/controls/_mapdata_spec.rb
+++ test/integration/default/controls/_mapdata_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 control '`map.jinja` YAML dump' do
-  title 'should contain the lines'
+  title 'should contain exactly the same data as the comparison file'
 
   mapdata_file = "_mapdata/#{system.platform[:finger].split('.').first}.yaml"
-  mapdata_dump = inspec.profile.file(mapdata_file)
+  mapdata_dump = YAML.safe_load(inspec.profile.file(mapdata_file))
 
-  describe file('/tmp/salt_mapdata_dump.yaml') do
-    it { should exist }
-    its('content') { should eq mapdata_dump }
+  describe yaml('/tmp/salt_mapdata_dump.yaml').params do
+    it { should eq mapdata_dump }
   end
 end
